940cd8703309d8938b83a1afa18b5b0ceb3c6179
oliver254 <oliver254@hotmail.fr>
2019-01-26 11:58:45 +0100

    [#2] : Add migration version

---
+++ src/EvolveDB/MigrationVersion.cs
@@ -0,0 +1,154 @@
+﻿using EvolveDB.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace EvolveDB
+{
+    public sealed class MigrationVersion : IComparable<MigrationVersion>, IComparable, IEquatable<MigrationVersion>
+    {
+        #region Champs
+        public static readonly MigrationVersion MaxVersion = new MigrationVersion(long.MaxValue.ToString());
+        public static readonly MigrationVersion MinVersion = new MigrationVersion("0");
+        private const string VersionPattern = @"\.(?=\d)";
+        private static readonly Regex _parser = new Regex(VersionPattern);
+        #endregion
+
+        #region Constructeurs
+        public MigrationVersion(string version)
+        {
+            if (string.IsNullOrEmpty(version))
+            {
+                throw new ArgumentNullException(nameof(version));
+            }
+            NormalizedVersion = version.Replace("_", ".");
+            string[] array = _parser.Split(NormalizedVersion);
+            Parts = array.Select(long.Parse).ToArray();
+        }
+        #endregion
+
+        #region Propriétés
+        public string NormalizedVersion
+        {
+            get;
+        }
+        public IList<long> Parts
+        {
+            get;
+        }
+        #endregion
+
+        #region Méthodes
+        public static bool operator !=(MigrationVersion v1, MigrationVersion v2)
+        {
+            return !(v1 == v2);
+        }
+        public static bool operator <(MigrationVersion v1, MigrationVersion v2)
+        {
+            if((object)v1 == null)
+            {
+                throw new ArgumentNullException(nameof(v1));
+            }
+            return (v1.CompareTo(v2) < 0);
+        }
+
+        public static bool operator <=(MigrationVersion v1, MigrationVersion v2)
+        {
+            if((object)v1 == null)
+            {
+                throw new ArgumentNullException(nameof(v1));
+            }
+            return (v1.CompareTo(v2) <= 0);
+        }
+
+        public static bool operator ==(MigrationVersion v1, MigrationVersion v2)
+        {
+            if (v2 is null)
+            {
+                return (v1 is null);
+            }
+
+            return ReferenceEquals(v2, v1) || v2.Equals(v1);
+        }
+        public static bool operator >(MigrationVersion v1, MigrationVersion v2)
+        {
+            if((object)v1 == null)
+            {
+                throw new ArgumentNullException(nameof(v1));
+            }
+            return (v1.CompareTo(v2) > 0);
+        }
+
+        public static bool operator >=(MigrationVersion v1, MigrationVersion v2)
+        {
+            if((object)v1 == null)
+            {
+                throw new ArgumentNullException(nameof(v1));
+            }
+            return (v1.CompareTo(v2) >= 0);
+        }
+        public int CompareTo(object obj)
+        {
+            if (obj == null)
+            {
+                return 1;
+            }
+            Version other = obj as Version;
+            if (other == null)
+            {
+                throw new ArgumentException(Strings.Arg_MustBeVersion);
+            }
+            return CompareTo(other);
+        }
+
+        public int CompareTo(MigrationVersion other)
+        {
+            if (object.ReferenceEquals(other, this))
+            {
+                return 0;
+            }
+            else if (other is null)
+            {
+                return 1;
+            }
+
+            using (IEnumerator<long> e1 = Parts.GetEnumerator())
+            using (IEnumerator<long> e2 = other.Parts.GetEnumerator())
+            {
+                while (e1.MoveNext())
+                {
+                    if (!e2.MoveNext())
+                    {
+                        return 1;
+                    }
+                    int comparison = e1.Current.CompareTo(e2.Current);
+                    if (comparison == 0)
+                    {
+                        continue;
+                    }
+                    return comparison;
+                }
+                return e2.MoveNext() ? -1 : 0;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as MigrationVersion);
+        }
+
+        public bool Equals(MigrationVersion other)
+        {
+            return CompareTo(other) == 0;
+        }
+
+        public override int GetHashCode()
+        {
+            return NormalizedVersion.GetHashCode();
+        }
+
+        public override string ToString() => NormalizedVersion;
+        #endregion
+    }
+}
---
+++ test/EvolveDB.Tests/MigrationVersionTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace EvolveDB.Tests
+{
+    public class MigrationVersionTests
+    {
+        public static IEnumerable<object[]> MigrationVersion_TestData()
+        {
+            yield return new object[] { new MigrationVersion("8"), new MigrationVersion("3_1"), 1 };
+            yield return new object[] { new MigrationVersion("3_1"), new MigrationVersion("3_1_1"), -1 };
+            yield return new object[] { new MigrationVersion("5_2"), new MigrationVersion("5_2"), 0 };
+            yield return new object[] { new MigrationVersion("6_1_1"), new MigrationVersion("6_1_2"), -1 };
+        }
+
+        [Fact]
+        public void InvalidFormatTest()
+        {
+            Assert.Throws<FormatException>(() => new MigrationVersion("1_2_2i"));
+        }
+
+        [Theory]
+        [MemberData(nameof(MigrationVersion_TestData))]
+        public void ComparisonTest(MigrationVersion v1, MigrationVersion v2, int result)
+        {
+            Assert.Equal(result, v1.CompareTo(v2));
+        }
+        [Fact]
+        public void ComparisonWithNullTest()
+        {
+            var v1 = new MigrationVersion("1");
+            MigrationVersion v2 = null;
+
+            Assert.Equal(1, v1.CompareTo(v2));
+        }
+        [Fact]
+        public void ComparisonWithSameTest()
+        {
+            var v1 = new MigrationVersion("2_1");
+
+            Assert.Equal(0, v1.CompareTo(v1));
+        }
+        [Fact]
+        public void EmptyVersionTest()
+        {
+            Assert.Throws<ArgumentNullException>(() => new MigrationVersion(string.Empty));
+        }
+        [Fact]
+        public void EqualityTest()
+        {
+            var v1 = new MigrationVersion("2_1_2_4");
+            var v2 = new MigrationVersion("2_1_2_4");
+
+            Assert.True(v1.Equals(v2));
+        }
+        [Fact]
+        public void InvalidComparisonTest()
+        {
+            var v1 = new MigrationVersion("2_4_5");
+
+            Assert.Throws<ArgumentException>(() => v1.CompareTo("2_1_4"));
+        }
+        [Fact]
+        public void NoEqualityTest()
+        {
+            var v1 = new MigrationVersion("2_1_2");
+            var v2 = new MigrationVersion("2_1_2_1");
+
+            Assert.False(v1.Equals(v2));
+        }
+        [Theory]
+        [InlineData("1")]
+        [InlineData("3_1_4")]
+        public void PartsTest(string version)
+        {
+            Assert.Equal(new MigrationVersion(version).Parts, version.Split('_').Select(long.Parse).ToList());
+        }
+    }
+}
